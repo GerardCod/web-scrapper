46d52158e66647bf1d282c3f302a73544f60eb86
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 14:23:38 -0500

    [MODIFY] finishing divs selection

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ if __name__ == '__main__':
   with open('econpy.html', 'r') as file:
     content = file.read()
     soup = BeautifulSoup(content, 'html.parser')
-    title = soup.find('titles')
-    if title:
-      print(title)
-      print(type(title))
-      print(title.name)
-      print(title.text)
-      print(title.get_text())
+
+    for element in soup.find_all('div'):
+      print(element, '\n')
