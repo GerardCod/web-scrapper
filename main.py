d731d29ee8f864d47e1e59c64e4581d1ada18aea
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 17:57:42 -0500

    [MODIFY] Finishing nodes creation and append to body

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ if __name__ == '__main__':
   with open('econpy.html', 'r') as file:
     content = file.read()
     soup = BeautifulSoup(content, 'html.parser')
-    div = soup.find('div', {'title': 'buyer-info'})
-    div['id'] = 'item01'
-    div['title'] = 'new-title'
-
-    div.div.string = 'GerardCod'
-    print(div.get('titles', 'default'))
-    print(div)
+    div = soup.new_tag('div', id='title', title='data')
+    a = soup.new_tag('a', href='www.codigofacilito.com')
+    a.string = 'Link a la plataforma'
+    div.append('\n')
+    div.append(a)
+    div.append('\n')
+    # soup.body.append(div)
+    soup.body.insert(1, div)
+    print(soup.prettify())
