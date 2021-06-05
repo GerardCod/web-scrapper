4b9c78ea9f40b38f7d1abb1a97fffd0c4a577cc2
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 12:00:28 -0500

    [MODIFY] Finishing substring technique

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ if __name__ == '__main__':
   response = requests.get(URL)
 
   if response.status_code == 200:
-    content = response.text
-    with open('econpy.html', 'w+') as file:
-      file.write(content) 
+    content = response.text # contenido de la página web
+
+    regexa = '<div title="buyer-name">'
+    regexb = '</div>'
+    for line in content.split('\n'):
+      if regexa in line:
+        start = line.find(regexa) + len(regexa)
+        end = line.find(regexb)
+        title = line[start:end]
+        print(title)
+    
