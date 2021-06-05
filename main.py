5683187c6f4dc9e6d7932a3c88be52ce450e54c5
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 18:15:18 -0500

    [MODIFY] Finishing delete and extraction of nodes

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ if __name__ == '__main__':
   with open('econpy.html', 'r') as file:
     content = file.read()
     soup = BeautifulSoup(content, 'html.parser')
-    div = soup.new_tag('div', id='title', title='data')
-    a = soup.new_tag('a', href='www.codigofacilito.com')
-    a.string = 'Link a la plataforma'
-    div.append('\n')
-    div.append(a)
-    div.append('\n')
-    # soup.body.append(div)
-    soup.body.insert(1, div)
-    print(soup.prettify())
+    div = soup.find('div', {'title': 'buyer-info'})
+    # div.contents = []
+    div.div.string = ''
+    span = div.span.extract()
+    print(div)
+    print(span)
