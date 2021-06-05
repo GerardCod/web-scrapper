425058e4324dada7e07608a272c9af76a84ceca8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 12:08:36 -0500

    [MODIFY] Finishing local file reading

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import requests
 import re
 
-URL = 'http://econpy.pythonanywhere.com/ex/001.html'
-
 if __name__ == '__main__':
-  response = requests.get(URL)
-
-  if response.status_code == 200:
-    content = response.text
+  with open('econpy.html', 'r') as file:
+    content = file.read()
     regex = '<div title="buyer-name">(.+?)</div>'
     for title in re.findall(regex, content):
       print(title)
