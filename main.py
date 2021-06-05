2787e3ba52a3681b47d0f41e9a3092a0da8e401f
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 12:25:02 -0500

    [MODIFY] Finishing title tag searching

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ if __name__ == '__main__':
   with open('econpy.html', 'r') as file:
     content = file.read()
     soup = BeautifulSoup(content, 'html.parser')
-    print(soup.head)
-    print(type(soup.head))
+    title = soup.find('titles')
+    if title:
+      print(title)
+      print(type(title))
+      print(title.name)
+      print(title.text)
+      print(title.get_text())
