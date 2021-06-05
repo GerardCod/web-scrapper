06ba0b53f8d9211a844d1e6c6cddaef60e4538a0
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-04 22:19:13 -0500

    [ADD] First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+URL = 'http://econpy.pythonanywhere.com/ex/001.html'
+
+if __name__ == '__main__':
+  response = requests.get(URL)
+
+  if response.status_code == 200:
+    content = response.text
+    with open('econpy.html', 'w+') as file:
+      file.write(content) 
