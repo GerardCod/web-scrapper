e58a8cbdace2d996efbcd8a75767f7434fea6235
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 20:34:17 -0500

    [MODIFY] News title extraction feature finished

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-URL = 'http://econpy.pythonanywhere.com/ex/001.html'
+URL = 'https://news.google.com/topics/CAAqLQgKIidDQkFTRndvSkwyMHZNR1ptZHpWbUVnWmxjeTAwTVRrYUFrMVlLQUFQAQ?hl=es-419&gl=MX&ceid=MX%3Aes-419'
 
 if __name__ == '__main__':
   response = requests.get(URL)
 
   if response.status_code == 200:
     content = response.text
-    with open('econpy.html', 'w+') as file:
-      file.write(content) 
+    soup = BeautifulSoup(content, 'html.parser')
+
+    now = datetime.now().strftime('%d_%m_%Y')
+    file_name = f'titles_{now}.txt'
+    with open(f'news/{file_name}', 'w+') as file:
+      for element in soup.find_all('h3', class_='ipQwMb ekueJc RD0gLb', limit=10):
+        title = element.a.text
+        file.write(title + '\n')
+      print(f'Archivo {file_name} generado exitosamente')
+    
