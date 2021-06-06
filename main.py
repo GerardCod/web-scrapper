6b31ba1de4e69df094404d34f64e730256f7c511
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-06-05 19:57:01 -0500

    [MODIFY] Finishing pokemon project

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import requests
 from bs4 import BeautifulSoup
 
+URL = '/pokedex/all'
+DOMAIN = 'https://pokemondb.net'
+
+def get_content(url):
+  response = requests.get(url)
+  if response.status_code == 200:
+    content = response.text
+    return BeautifulSoup(content, 'html.parser')
+  else:
+    return None
+
+def get_species(url):
+  soup = get_content(url)
+  pokemon_table = soup.find('table', class_='vitals-table')
+  species = pokemon_table.tbody.find_all('tr')[2].td.text
+  return species
+  
+
+def show_pokemon_data():
+  soup = get_content(DOMAIN + URL)
+  table = soup.find('table', {'id': 'pokedex'})
+    
+  for row in table.tbody.find_all('tr', limit=5):
+    columns = row.find_all('td', limit=3)
+    name = columns[1].a.text
+    types = [a.text for a in columns[2].find_all('a')] # List comprehension
+    link = DOMAIN + columns[1].a['href']
+    species = get_species(link)
+    print(name, '-', *types, '-', species)
+
+
 if __name__ == '__main__':
-  with open('econpy.html', 'r') as file:
-    content = file.read()
-    soup = BeautifulSoup(content, 'html.parser')
-    div = soup.find('div', {'title': 'buyer-info'})
-    # div.contents = []
-    div.div.string = ''
-    span = div.span.extract()
-    print(div)
-    print(span)
+  show_pokemon_data()
